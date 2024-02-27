8ec1cc645595a2c4b6c30d458e1b520c7ce75240
Johannes Krainer <j.krainer@posteo.de>
2024-02-27 17:19:00 +0100

    fixed macro usage

--- pkg/gpiod/device.go
+++ pkg/gpiod/device.go
@@ -23,7 +23,7 @@ func (d *device) Open() error {
 	charC := C.CString(d.path)
 	defer C.free(unsafe.Pointer(charC))
 	structC := C.gpiod_chip_open(charC)
-	if structC == C.NULL {
+	if structC == 0 {
 		return fmt.Errorf("%s failed: NULL returned", "gpiod_chip_open")
 	}
 	d.nativeRef = structC
