8b5bde12c3ff3c0f6b958702aa71a829ee423314
Johannes Krainer <j.krainer@posteo.de>
2024-02-27 17:20:46 +0100

    fixed variable assignment

--- pkg/gpiod/device.go
+++ pkg/gpiod/device.go
@@ -22,10 +22,10 @@ func NewDevice(path string) *device {
 func (d *device) Open() error {
 	charC := C.CString(d.path)
 	defer C.free(unsafe.Pointer(charC))
-	structC := C.gpiod_chip_open(charC)
-	if structC == 0 {
+	var nativeRef *C.struct_gpiod_chip = C.gpiod_chip_open(charC)
+	if nativeRef == 0 {
 		return fmt.Errorf("%s failed: NULL returned", "gpiod_chip_open")
 	}
-	d.nativeRef = structC
+	d.nativeRef = nativeRef
 	return nil
 }
